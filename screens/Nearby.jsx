673947b69e53446e40c26365ef5387d0c9dc4eb8
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-07-26 17:05:37 +0530

    fetch live data from firebase live database

--- screens/Nearby.jsx
+++ screens/Nearby.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
+import { getLocation } from "../firebase/worker";
 
-const NearbyScreen = () => {
-  const { deviceId: id } = Constants;
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text>{id}</Text>
-      </View>
-    </ScrollView>
-  );
-};
+class NearbyScreen extends React.Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    getLocation().then((data) => {
+      let x;
+      const users = [];
+      for (x in data.device) {
+        const { latitude, longitude } = data.device[x].location;
+        users.push({
+          id: x,
+          latitude,
+          longitude,
+        });
+      }
+      this.setState({ users });
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Total Users : {this.state.users.length}</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
